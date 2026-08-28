573cd9ce08df3b9216cf955efd3bfb80bbbadb7c
ROOT <enesmisirlioglu0@icloud.com>
2026-08-28 06:31:24 +0300

    Initial Commit

---
+++ IPanel/IPanelApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPanelApp.swift
+//  IPanel
+//
+//  Created by ENES MISIRLIOĞLU on 28.08.2026.
+//
+
+import SwiftUI
+
+@main
+struct IPanelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
